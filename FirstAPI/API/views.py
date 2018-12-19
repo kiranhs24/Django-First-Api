4f37cf3cdf70e3a5c7eb3a16938ac7a78718a213
kiranhs24 <hskiran3@gmail.com>
2018-12-19 10:00:19 +0530

    Created First Django Api

--- FirstAPI/API/views.py
+++ FirstAPI/API/views.py
@@ -1,23 +1,22 @@
 from django.shortcuts import render
-
 # Create your views here.
+
+import json
 
 from django.http import HttpResponse
 
 # HttpResponse is used to render html (not the best way beacuse we have hard coded html in the view)
 
 def hello(request):
-   text = """<h1>welcome to first application </h1>"""
-   return HttpResponse(text)
+   msg = """Hi, U r inside ur first application """
+   return HttpResponse(msg)
+
 
 '''
 from django.shortcuts import render
-
 def hello(request):
-   return render(request, "API/template/hello.html", {})
+   return render(request, "FirstAPI/FirstAPI/API/hello.html", {})
 '''
-
 
 # We have used MVT format instead of hard coded html
 # we have rendered hello.html directly to web page
-
--- FirstAPI/settings.py
+++ FirstAPI/settings.py
@@ -9,10 +9,11 @@ https://docs.djangoproject.com/en/2.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/2.0/ref/settings/
 """
-
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
+from django.views.decorators import http
+
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
@@ -120,3 +121,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
